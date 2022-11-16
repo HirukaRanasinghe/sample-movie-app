a8e4761da4249ee3092898cb71ad7096fbaf3a15
HirukaRanasinghe <62961312+HirukaRanasinghe@users.noreply.github.com>
2022-11-16 11:01:09 +0530

    Add Service Worker

--- sample-movie-app/src/app/app.module.ts
+++ sample-movie-app/src/app/app.module.ts
@@ -13,6 +13,7 @@ import {reducers, metaReducers} from './store/app.reducer';
 import {environment} from '../environments/environment';
 
 import {AppComponent} from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 @NgModule({
@@ -32,7 +33,13 @@ import {AppComponent} from './app.component';
             }
         }),
         EffectsModule.forRoot([...Effects]),
-        StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production})
+        StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: environment.production,
+          // Register the ServiceWorker as soon as the app is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
